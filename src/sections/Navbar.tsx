f1baeb732685d25db67a4e361140ab16f32b710f
parvez <parvezz13913@gmail.com>
2025-07-11 22:50:16 +0600

    [DONE] Navbar

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,8 +1,30 @@
 "use client";
 
+import { navLinks } from "@/constans";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
+
+const NavItems = () => {
+  return (
+    <ul className="flex flex-col items-center gap-4 sm:flex-row md:gap-6 relative z-20">
+      {navLinks.map(({ id, name, href }) => (
+        <li
+          key={id}
+          className="text-neutral-400 hover:text-white font-generalsans max-sm:hover:bg-black-500 max-sm:w-full max-sm:rounded-md py-2 max-sm:px-5"
+        >
+          <Link
+            href={href}
+            className="text-lg md:text-base hover:text-white transition-colors"
+            onClick={() => {}}
+          >
+            {name}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -30,7 +52,20 @@ const Navbar = () => {
               className="h-6 w-6"
             />
           </button>
+
+          <nav className="sm:flex hidden">
+            <NavItems />
+          </nav>
         </div>
+      </div>
+      <div
+        className={`absolute left-0 right-0 bg-black-200 backdrop-blur-sm transition-all duration-300 ease-in-out overflow-hidden z-20 mx-auto sm:hidden block ${
+          isOpen ? "max-h-screen" : "max-h-0"
+        }`}
+      >
+        <nav className="p-5 ">
+          <NavItems />
+        </nav>
       </div>
     </header>
   );
